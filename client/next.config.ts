55698cb0136decfe3618247432b0cfbff2f76951
hurrinade <marko.uremovic00@gmail.com>
2025-08-01 19:01:27 +0200

    adjusted dev/docker/production run of application, and application build for production on render

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,12 +1,22 @@
 import type { NextConfig } from "next";
 
+const isProduction = process.env.NODE_ENV === "production";
+const isTest = process.env.NODE_ENV === "test";
+
+let url = "http://localhost:3001";
+if (isProduction) {
+  url = "https://authentication-server-hvph.onrender.com";
+} else if (isTest) {
+  url = "http://dev-server:3000";
+}
+
 const nextConfig: NextConfig = {
   /* config options here */
   async rewrites() {
     return [
       {
         source: "/api/v1/:path*",
-        destination: "http://dev-server:3000/api/v1/:path*",
+        destination: url + "/api/v1/:path*",
       },
     ];
   },
